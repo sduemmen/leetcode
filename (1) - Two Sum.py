6fa037edd5aaab8dfc4ddd8c6002bb610c68ac81
sduemmen <sduemmentudents.uni-mainz.de>
2021-06-23 13:17:37 +0200

    second solution

--- (1) - Two Sum.py
+++ (1) - Two Sum.py
@@ -1,12 +1,10 @@
+# 8624 ms	14.5 MB
 def twoSum(nums, target):
-
     """
     :type nums: List[int]
     :type target: int
     :rtype: List[int]
     """
-    if len(nums) == 2:
-        return [0,1]
     for i in range(len(nums)):
         for j in range(len(nums)):
             if i == j:
@@ -14,9 +12,3 @@ def twoSum(nums, target):
             else:
                 if nums[i] + nums[j] == target:
                     return [i, j]
-
-
-print(twoSum(nums = [2,7,11,15], target = 9))
-print(twoSum(nums = [3,2,4], target = 6))
-print(twoSum(nums = [3,3], target = 6))
-print(twoSum(nums = [-1,-2,-3,-4,-5], target = -8))
