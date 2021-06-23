5c551d1e3f1a7942e6233e8d22f0d33e99059e97
sduemmen <sduemmentudents.uni-mainz.de>
2021-06-23 13:22:41 +0200

    third solution

--- (1) - Two Sum.py
+++ (1) - Two Sum.py
@@ -1,14 +1,12 @@
-# 8624 ms	14.5 MB
+# 3648 ms	14.5 MB
 def twoSum(nums, target):
     """
     :type nums: List[int]
     :type target: int
     :rtype: List[int]
     """
-    for i in range(len(nums)):
-        for j in range(len(nums)):
-            if i == j:
-                pass
-            else:
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+    length = len(nums)
+    for i in range(length):
+        for j in range(i+1, length):
+            if nums[i] + nums[j] == target:
+                return [i, j]
