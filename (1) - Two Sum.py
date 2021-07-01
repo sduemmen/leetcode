9b70fa0c5736449c0caca53fa30ea007ac89c193
Samuel Dümmen <samuel.duemmen@gmx.de>
2021-07-01 04:47:49 +0200

    third solution

--- (1) - Two Sum.py
+++ (1) - Two Sum.py
@@ -1,10 +1,5 @@
 # 3648 ms	14.5 MB
-def twoSum(nums, target):
-    """
-    :type nums: List[int]
-    :type target: int
-    :rtype: List[int]
-    """
+def twoSum(self, nums: list[int], target: int) -> list[int]:
     length = len(nums)
     for i in range(length):
         for j in range(i+1, length):
