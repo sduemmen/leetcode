1741ebc413ec720511cca7bbf37b909ebd7516fc
Samuel Dümmen <samuel.duemmen@gmx.de>
2021-07-07 17:37:39 +0200

    update progress

--- progress.py
+++ progress.py
@@ -1,10 +1,8 @@
 import glob
 
 fileNames = glob.glob("*/*.py")
-
 fileCount = len(fileNames)
-
 progress = fileCount // 1923
 
 with open("README.md", "w") as file:
-    file.write("# leetcode\n\n<p1>a collection of [leetcode.com](leetcode.com) challenges and solutions</p1>\n\n![" + str(progress) + "%](https://progress-bar.dev/" + str(progress) + ")")
+    file.write("# leetcode\n\n<p1>a collection of [leetcode.com](leetcode.com) challenges and solutions</p1>\n\n![" + str(progress) + "%](https://progress-bar.dev/" + str(progress) + "/?scale=500&title=solved&width=120)")
