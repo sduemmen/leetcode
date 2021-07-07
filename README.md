0dc60a1077d54398113f8d9d48487af2943d282a
Samuel Dümmen <samuel.duemmen@gmx.de>
2021-07-07 17:31:59 +0200

    added progress tracker

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 <p1>a collection of [leetcode.com](leetcode.com) challenges and solutions</p1>
 
-![0.005200208008320333%](https://progress-bar.dev/0.005200208008320333)
+![0%](https://progress-bar.dev/0)
--- progress.py
+++ progress.py
@@ -4,7 +4,7 @@ fileNames = glob.glob("*/*.py")
 
 fileCount = len(fileNames)
 
-progress = fileCount / 1923
+progress = fileCount // 1923
 
 with open("README.md", "w") as file:
     file.write("# leetcode\n\n<p1>a collection of [leetcode.com](leetcode.com) challenges and solutions</p1>\n\n![" + str(progress) + "%](https://progress-bar.dev/" + str(progress) + ")")
