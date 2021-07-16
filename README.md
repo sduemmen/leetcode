3712e2ff3e64ee6bae31d1781b40beb53badbe82
Samuel Dümmen <samuel.duemmen@gmx.de>
2021-07-16 10:26:13 +0200

    updated progress

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 <p1>challenges from [leetcode.com](leetcode.com)</p1>
 
-![0%](https://progress-bar.dev/0/?scale=500&title=solved&width=330)
+![1%](https://progress-bar.dev/1/?scale=500&title=solved&width=330)
--- progress.py
+++ progress.py
@@ -2,7 +2,10 @@ import glob
 
 fileNames = glob.glob("*/*.py")
 fileCount = len(fileNames)
-progress = fileCount // 1923
+progress = round(fileCount / 1923 * 100)
 
 with open("README.md", "w") as file:
-    file.write("# leetcode\n\n<p1>teaching myself dynamic programming</p1>\n\n<p1>challenges from [leetcode.com](leetcode.com)</p1>\n\n![" + str(progress) + "%](https://progress-bar.dev/" + str(progress) + "/?scale=500&title=solved&width=330)")
+    file.write("# leetcode\n\n"
+    "<p1>teaching myself dynamic programming</p1>\n\n"
+    "<p1>challenges from [leetcode.com](leetcode.com)</p1>\n\n"
+    "![" + str(progress) + "%](https://progress-bar.dev/" + str(progress) + "/?scale=500&title=solved&width=330)")
