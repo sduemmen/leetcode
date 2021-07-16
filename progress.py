78a83a5c56c0c71e7f818c711fbc6777faf0c35d
Samuel Dümmen <samuel.duemmen@gmx.de>
2021-07-16 10:19:25 +0200

    updated progress

--- progress.py
+++ progress.py
@@ -5,4 +5,4 @@ fileCount = len(fileNames)
 progress = fileCount // 1923
 
 with open("README.md", "w") as file:
-    file.write("# leetcode\n\n<p1>a collection of [leetcode.com](leetcode.com) challenges and solutions</p1>\n\n![" + str(progress) + "%](https://progress-bar.dev/" + str(progress) + "/?scale=500&title=solved&width=330)")
+    file.write("# leetcode\n\n<p1>teaching myself dynamic programming</p1>\n\n<p1>challenges from [leetcode.com](leetcode.com)</p1>\n\n![" + str(progress) + "%](https://progress-bar.dev/" + str(progress) + "/?scale=500&title=solved&width=330)")
