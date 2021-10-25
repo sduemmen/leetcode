ba138d3ee0e9c44e3ea03bf2167e3187b4d4ddd4
Samuel Dümmen <samuel.duemmen@gmx.de>
2021-10-25 08:24:40 +0200

    updated progress tracker

--- README.md
+++ README.md
@@ -4,4 +4,6 @@
 
 <p1>challenges from [leetcode.com](leetcode.com)</p1>
 
+18/2050 Python Solutions
+6/2050 Javascript Solutions
 ![1%](https://progress-bar.dev/1/?scale=500&title=solved&width=330)
--- progress.py
+++ progress.py
@@ -1,11 +1,18 @@
 import glob
+allProblems = 2050
 
-fileNames = glob.glob("*/*.py")
-fileCount = len(fileNames)
-progress = round(fileCount / 1923 * 100)
+fileNamesPy = glob.glob("*/*.py")
+fileCountPy = len(fileNamesPy)
+
+fileNamesJs = glob.glob("*/*.js")
+fileCountJs = len(fileNamesJs)
+
+progress = round(fileCountPy / allProblems * 100)
 
 with open("README.md", "w") as file:
     file.write("# leetcode\n\n"
     "<p1>teaching myself dynamic programming</p1>\n\n"
     "<p1>challenges from [leetcode.com](leetcode.com)</p1>\n\n"
+    f'{fileCountPy}/{allProblems} Python Solutions\n'
+    f'{fileCountJs}/{allProblems} Javascript Solutions\n'
     "![" + str(progress) + "%](https://progress-bar.dev/" + str(progress) + "/?scale=500&title=solved&width=330)")
